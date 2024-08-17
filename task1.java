543dcf7467dcce9276b922d00d1668a2175fda78
CrissCross-max <xex3malx777777@gmail.com>
2024-08-18 01:20:42 +0700

    Add files via upload

---
+++ task1.java
@@ -0,0 +1,12 @@
+public class task1 {
+    public static void Main(String[] args){
+        int x = 10;
+        int y = 15;
+
+        int makemath1 = x/y;
+        int makemath2 = x%y;
+        int makemath3 = sqrt(x);
+        System.out.printLn;
+
+    }
+}
